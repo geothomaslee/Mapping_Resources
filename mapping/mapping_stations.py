c3ced5ccef1ce69a5d5c30cf091a712c47de3537
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-07 15:59:23 -0700

    Update mapping_stations.py

--- mapping/mapping_stations.py
+++ mapping/mapping_stations.py
@@ -7,6 +7,7 @@ Created on Tue Nov  7 15:05:26 2023
 
 import obspy
 from obspy.clients.fdsn import Client
+import pygmt
 
 def find_stations(network, starttime,endtime,station='*',client="IRIS"):
     working_client = Client(client)
@@ -19,9 +20,6 @@ def find_stations(network, starttime,endtime,station='*',client="IRIS"):
 
 def find_multi_network(deployment_list,client="IRIS"):
     working_client = Client(client)
-
-    
-    
     for i, deployment in enumerate(deployment_list):
         network = deployment[0]
         starttime = deployment[1]
@@ -39,12 +37,92 @@ def find_multi_network(deployment_list,client="IRIS"):
                                                         network=network, 
                                                         station='*',
                                                         level="station")
-            station_inv = station_inv + secondary_inv
+            station_inv += secondary_inv
             
     return station_inv
             
       
 station_inv = find_stations("ZR","2015-01-01","2017-12-31")
 station_inv.plot()
+        
+deployment_list = [["ZR","2015-01-01","2017-12-31"]]
+
+station_inv = find_multi_network(deployment_list)
+station_inv.plot()
+
+def get_coordinates_from_network(network):
+    lat_list = []
+    lon_list = []
+    elev_list = []
+    
+    for station in network:
+        lat_list.append(station.latitude)
+        lon_list.append(station.longitude)
+        elev_list.append(station.elevation)
+        
+    return lat_list, lon_list, elev_list
+    
+def get_coordinate_list(inventory):
+    lat_list = []
+    lon_list = []
+    elev_list = []
+    
+    for network in inventory:
+        lats, lons, elevs = get_coordinates_from_network(network)
+        lat_list += lats
+        lon_list += lons
+        elev_list += elevs
+    
+    return lat_list, lon_list, elev_list
+
+def check_lon(coord):
+    """Checks if coordinates are -180 < coord < 180 and wraps if out of range"""
+    if coord < -180:
+        coord = 180 + (coord - -180)
+    elif coord > 180:
+        coord = -180 + (coord - 180)
+    return coord
+
+def check_lat(coord):
+    """Checks if latitudes are in range -90 < coordinate < 90"""
+    if coord < -90:
+        coord = -90
+    elif coord > 90:
+        coord = 90
+    return coord
+
+def get_map_bounds(lats,lons,margin=0.1):
+    min_lon = min(lons) - (margin * abs(max(lons) - min(lons)))
+    min_lat = min(lats) - (margin * abs(max(lats) - min(lats)))
+    max_lon = max(lons) + (margin * abs(max(lons) - min(lons)))
+    max_lat = max(lats) + (margin * abs(max(lats) - min(lats)))
+    
+    min_lon = check_lon(min_lon)
+    min_lat = check_lat(min_lat)
+    max_lon = check_lon(max_lon)
+    max_lat = check_lat(max_lat)
+    
+    bounds = [min_lon, max_lon, min_lat, max_lat]
+    
+    return bounds
+
+def plot_stations:
+    fig = pygmt.Figure()
+    fig.basemap(region=region,
+                 projection=PROJECTION)
+    fig.grdimage(grid=grid,
+                 projection=PROJECTION,
+                 frame=["a",f'+t{FIGURE_NAME}'],
+                 cmap='dem2')
+    fig.coast(shorelines="4/0.5p,black",
+              projection=PROJECTION,
+              borders="1/1.2p,black",
+              water="skyblue",
+              resolution="f")
+    
+plot)stations
+    
+
+        
         
         

29ec8e8fcb53c16de6eabb7f5089e24399dd9f95
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-10 09:32:41 -0700

    Update Mapping_Main.py
    Main now more readable

--- Mapping_Main.py
+++ Mapping_Main.py
@@ -11,9 +11,7 @@ import scripts.general_mapping as gm
 
 deployment_list = [["UW","2015-01-01","2017-12-31"],["XU","2007-01-01","2011-12-31"],
                    ["XD","2014-01-01","2016-12-31","M*"],["TA","2006-01-01","2023-11-6"],
-                   #["YW","2005-08-11","2005-12-31"],
-                   ["CC","2020-10-22","2023-11-7"],["YH","2016-01-01","2016-12-31"]
-                   ]
+                   ["CC","2020-10-22","2023-11-7"],["YH","2016-01-01","2016-12-31"]]
 
 region_bounds = [-122.5, -120.5, 45, 47.25]
 box_bounds = [-122.5, -120.9, 46.1, 47.3]
@@ -31,15 +29,17 @@ regional_fig.show()
 gm.save_fig(regional_fig,"PNW Regional Overview")
      
 # Plotting a narrower regional interview        
-fig = ms.plot_stations(station_inv,
-                       figure_name="Rainier Region Seismic Stations",
-                       box_bounds=box_bounds)
-fig.show()
-gm.save_fig(fig,"Regional_Stations")
+rainier_region_fig = ms.plot_stations(station_inv,
+                                      figure_name="Rainier Region Seismic Stations",
+                                      box_bounds=box_bounds)
+                       
+rainier_region_fig.show()
+gm.save_fig(rainier_region_fig,"Regional_Stations")
 
 # Plotting the bounding box
-fig2 = ms.plot_stations(station_inv,region=box_bounds,
-                        figure_name="Stations within Bounding Box",
-                        box_bounds=box_bounds)
-fig2.show()
-gm.save_fig(fig2,"Local_View_Stations")
+local_fig = ms.plot_stations(station_inv,region=box_bounds,
+                             figure_name="Stations within Bounding Box",
+                             box_bounds=box_bounds)
+                        
+local_fig.show()
+gm.save_fig(local_fig,"Local_View_Stations")

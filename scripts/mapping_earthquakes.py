3624595bd86e492e41ebb7e9092e09c2b4d47f32
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-13 08:42:02 -0700

    Create mapping_earthquakes

---
+++ scripts/mapping_earthquakes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 08:35:58 2023
+
+@author: tlee4
+"""
+
+import pygmt
+import obspy
+
+if __name__ == '__main__':
+    import general_mapping as gm
+else:
+    import scripts.general_mapping as gm
+    
+
+
+def find_events_in_region(region):
+    pass
+
+def find_events_around_point(pointlon,pointlat):
+    pass
+
+def plot_events(args):
+    pass

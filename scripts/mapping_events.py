4140e03d4a971c6d6e338a5a2689bca970345da8
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2023-11-13 08:56:48 -0700

    Update mapping_events.py

--- scripts/mapping_events.py
+++ scripts/mapping_events.py
@@ -6,7 +6,7 @@ Created on Mon Nov 13 08:35:58 2023
 """
 
 import pygmt
-import obspy
+from obspy.clients.fdsn import Client
 
 if __name__ == '__main__':
     import general_mapping as gm
@@ -15,8 +15,8 @@ else:
     
 
 
-def find_events_in_region(region):
-    pass
+def find_events_in_region(region,client='IRIS'):
+    working_client=Client(client)
 
 def find_events_around_point(pointlon,pointlat):
     pass

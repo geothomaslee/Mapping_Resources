100a4fd7de2516d60723a4a9e5a3dc9aee81c60e
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2024-01-25 10:07:26 -0700

    Update station_utils

--- scripts/station_utils.py
+++ scripts/station_utils.py
@@ -9,6 +9,53 @@ Created on Thu Jan 25 10:02:18 2024
 import pandas as pd
 import obspy
 
+def get_station_df(inventory):
+    """
+    Parameters
+    ----------
+    inventory : obspy.core.inventory.Inventory
+        ObsPy station inventory.
+
+    Returns
+    -------
+    stat_df : pandas.DataFrame
+        DataFrame containing the station info. To get a CSV, use 
+        station_utils.get_station_csv
+
+    """
+    networks = []
+    stations = []
+    start_dates = []
+    end_dates = []
+    latitudes = []
+    longitudes = []
+
+
+    for network in inventory:
+        for station in network:
+            networks.append(network.code)
+            stations.append(station.code)
+            start_dates.append(station.start_date)
+            
+            end_date = station.end_date
+            if end_date:
+                end_dates.append(end_date)
+            else:
+                end_dates.append('Current')
+                
+            latitudes.append(station.latitude)
+            longitudes.append(station.longitude)
+
+
+    stat_dict = {'Network' : networks, 'Station' : stations,
+                 'Latitudes' : latitudes, 'Longitudes' : longitudes,
+                 'Start Date' : start_dates, 'End Date' : end_dates}
+
+    stat_df = pd.DataFrame(stat_dict)
+
+    return stat_df
+
+
 def get_station_csv(inventory,filename='Stations.csv'):
     """
     Parameters
@@ -54,3 +101,13 @@ def get_station_csv(inventory,filename='Stations.csv'):
     stat_df = pd.DataFrame(stat_dict)
     stat_df.to_csv(filename)
     
+    
+def station_availability_from_csv(filepath):
+    if type(filepath) != str:
+        raise TypeError('Expected type string for filepath to csv')
+        
+    
+        
+
+        
+station_availability_from_csv('./Stations_To_Correlate.csv')

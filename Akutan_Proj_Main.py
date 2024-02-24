8bff4e7bec73e5d37320729a022cc57b9337b772
Thomas J. Lee <117687003+geothomaslee@users.noreply.github.com>
2024-02-23 21:48:14 -0700

    update mapping_gps

--- Akutan_Proj_Main.py
+++ Akutan_Proj_Main.py
@@ -5,17 +5,15 @@ Created on Tue Feb 20 14:45:59 2024
 @author: tlee4
 """
 
-import scripts.mapping_stations as ms
 import scripts.general_mapping as gm
 import scripts.mapping_gps as mg
 import scripts.colormap_utils as cu
-import os
-
 
 akutan_region = [-167.8, -162.6, 53.2, 55.1]
 akutan_island_bounds = [-166.1464,-165.6164,54.0402,54.2301]
 projection = 'J-65/12c'
         
+#-----------------------------------
 
 ak_dir =  '~/Documents/Grad School/Spring 2024/PHYS581/Alaska_Project/Xue_Data/gps'
 stat_files = mg.find_files_in_bounds(ak_dir,akutan_island_bounds,'.pfiles')
@@ -26,10 +24,14 @@ akutan_island_fig = gm.plot_base_map(akutan_island_bounds,projection=projection,
                                      resolution='03s',cmap=cmap,figure_name='Akutan Island',
                                      bathymetry=True, colorbar_tick=500)
 
-akutan_island_fig = mg.plot_gps_stations(akutan_island_fig,stat_df)
+akutan_island_fig = mg.plot_gps_displacement_vectors(akutan_island_fig,stat_df,
+                                                     scaling_factor=1000000)
+
+#akutan_island_fig = mg.plot_gps_stations(akutan_island_fig,stat_df)
 
 akutan_island_fig.show()
 
+"""
 #------------------------------------
 
 cmap = cu.create_combined_color_map('usgs','colombia',max_elev=2000,max_depth=-8000)
@@ -59,5 +61,5 @@ aleutian_regional_fig = gm.plot_holocene_volcanoes(aleutian_regional_fig)
 aleutian_regional_fig = gm.plot_major_cities(aleutian_regional_fig,offset=0.05)
 
 aleutian_regional_fig.show()
-
+"""
 
--- scripts/mapping_gps.py
+++ scripts/mapping_gps.py
@@ -8,6 +8,7 @@ Created on Thu Feb 22 14:09:01 2024
 import os
 import pandas as pd
 from glob import glob
+import numpy as np
 
 
 def find_files_in_bounds(directory,bounds,filetype=None):
@@ -60,7 +61,7 @@ def make_gps_station_df(filelist):
     e_disp_list = []
     n_disp_list = []
     h_disp_list = []
-    
+
     if '.pfiles' in file_name:
         cols = ['Time','Station_Name','Period?','Longitude','Latitude','Height','E_Uncer','N_Uncer','H_Uncer',
                 'E-N_Corr,','E-H_Corr','N-H_Corr']
@@ -70,19 +71,24 @@ def make_gps_station_df(filelist):
             stat_list.append(stat_df.iloc[0,1])
             lon_list.append(stat_df.iloc[0,3])
             lat_list.append(stat_df.iloc[0,4])
+            e_disp_list.append(stat_df.iloc[-1,3] - stat_df.iloc[0,3])
+            n_disp_list.append(stat_df.iloc[-1,4] - stat_df.iloc[0,4])
+            h_disp_list.append(stat_df.iloc[-1,5] - stat_df.iloc[0,5])
             
-        stat_df = pd.DataFrame({'Station' : stat_list,
-                                'Longitude' : lon_list,
-                                'Latitude' : lat_list})
-        
+    stat_df = pd.DataFrame({'Station' : stat_list,
+                            'Longitude' : lon_list,
+                            'Latitude' : lat_list,
+                            'E_Disp' : e_disp_list,
+                            'N_Disp' : n_disp_list,
+                            'H_Disp': h_disp_list})
+    
     return stat_df
-        
         
 def plot_gps_stations(fig,stat_df,fill='black'):
     
     xvals = stat_df['Longitude']
     yvals = stat_df['Latitude']
-    
+
     fig.plot(x=xvals,
              y=yvals,
              style='t0.35c',
@@ -90,14 +96,28 @@ def plot_gps_stations(fig,stat_df,fill='black'):
     
     return fig
 
+def plot_gps_displacement_vectors(fig,stat_df,scaling_factor=1000):
+    lats = stat_df['Latitude'].tolist()
+    lons = stat_df['Longitude'].tolist()
+    e_disp = stat_df['E_Disp'].tolist()
+    n_disp = stat_df['N_Disp'].tolist()
     
-
+    angle_list = []
+    length_list = []
+    for i in range(len(e_disp)):
+        angle_list.append(np.rad2deg(np.arctan(n_disp[i]/e_disp[i])))
+        length_list.append((e_disp[i]**2 + n_disp[i]**2)**0.5)
+        
+    length_list = [val*scaling_factor for val in length_list]
         
 
+    fig.plot(x=lons,
+             y=lats,
+             style="v0.6c+e",
+             direction=[angle_list, length_list],
+             pen="2p",
+             color="red3")
+            
 
-        
-
+    return fig
     
-        
-
-
